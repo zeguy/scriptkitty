ea3afc808823ee6880b9bcf5c0f9c674774b1cb8
zeguy <sem001@g.harvard.edu>
2019-09-23 14:54:56 -0400

    first commit

---
+++ feedsearch.py
@@ -0,0 +1,16 @@
+#!/bin/env python3 
+import urllib.request, sys
+#enter feeds
+feeds = ['https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt','https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt']
+results = []
+query = sys.argv[1]
+
+for f in feeds:
+    feed = urllib.request.urlopen(f)
+    #decode bytes into string based on response charset
+    content = feed.read().decode('utf-8')
+    if content.find(query) != -1:
+        print(query + "found in" + f)
+  
+print(results)
+
