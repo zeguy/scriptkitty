3fad9b9c27dae07a59413672ab9e8fcce0fc7f16
zeguy <sem001@g.harvard.edu>
2019-09-23 18:08:54 -0400

    cleanup output

--- feedsearch.py
+++ feedsearch.py
@@ -1,7 +1,7 @@
 #!/bin/env python3 
 import urllib.request, sys
 #enter feeds
-feeds = ['https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt','https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt']
+feeds = ['https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt']
 results = []
 query = sys.argv[1]
 
@@ -10,7 +10,5 @@ for f in feeds:
     #decode bytes into string based on response charset
     content = feed.read().decode('utf-8')
     if content.find(query) != -1:
-        print(query + "found in" + f)
-  
-print(results)
+        print(query + " found in " + f)
 
